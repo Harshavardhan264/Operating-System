f48f97ce5fd1186961957859b53d28c30dd1db77
Endala Harshavardhan <endalaharshavardhan@gmail.com>
2025-03-25 21:00:38 +0530

    Round Robin

--- Round Robin.c
+++ Round Robin.c
@@ -1,83 +1,66 @@
 #include <stdio.h>
 
-void roundRobinScheduling(int n, int bu[], int t) {
-    int wa[10], tat[10], ct[10], temp = 0, max = 0;
-    float awt = 0, att = 0;
-    // Finding the maximum burst time
-    for (int i = 0; i < n; i++) {
-        ct[i] = bu[i];
-        if (bu[i] > max) {
-            max = bu[i];
-        }
+int main() {
+    int i, j, n, bu[10], wa[10], tat[10], t, ct[10], remaining;
+    float awt = 0, att = 0, temp = 0;
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    remaining = n; 
+    for (i = 0; i < n; i++) {
+        printf("Enter Burst Time for process %d: ", i + 1);
+        scanf("%d", &bu[i]);
+        ct[i] = bu[i]; 
     }
-    // Round Robin Scheduling
-    for (int time = 0; time <= max; time += t) {
-        for (int i = 0; i < n; i++) {
-            if (bu[i] != 0) {
-                if (bu[i] <= t) {
-                    tat[i] = temp + bu[i];
+
+    printf("Enter the time slice: ");
+    scanf("%d", &t);
+    while (remaining > 0) {
+        for (i = 0; i < n; i++) {
+            if (bu[i] > 0) {
+                if (bu[i] <= t) { 
+                    tat[i] = temp + bu[i]; 
                     temp += bu[i];
-                    bu[i] = 0;
+                    bu[i] = 0; 
+                    remaining--; 
                 } else {
-                    bu[i] -= t;
+                    bu[i] -= t; 
                     temp += t;
                 }
             }
         }
     }
-    for (int i = 0; i < n; i++) {
+
+    // Calculate Waiting Time
+    for (i = 0; i < n; i++) {
         wa[i] = tat[i] - ct[i];
         att += tat[i];
         awt += wa[i];
     }
-    printf("\nThe Average Turnaround Time is -- %.2f", att / n);
-    printf("\nThe Average Waiting Time is -- %.2f", awt / n);
-    printf("\n\tPROCESS\t BURST TIME \t WAITING TIME\tTURNAROUND TIME\n");
 
-    for (int i = 0; i < n; i++) {
-        printf("\t%d \t %d \t\t %d \t\t %d \n", i + 1, ct[i], wa[i], tat[i]);
-    }
-}
+    // Display Results
+    printf("\nThe Average Turnaround Time is: %.2f", att / n);
+    printf("\nThe Average Waiting Time is: %.2f", awt / n);
 
-int main() {
-    int n, t;
-    
-    printf("Enter the number of processes: ");
-    scanf("%d", &n);
-
-    int bu[n]; 
-    for (int i = 0; i < n; i++) {
-        printf("\nEnter Burst Time for process %d: ", i + 1);
-        scanf("%d", &bu[i]);
-    }
-
-    // Input time quantum
-    printf("\nEnter the size of time slice: ");
-    scanf("%d", &t);
-
-    roundRobinScheduling(n, bu, t);
+    printf("\n\nPROCESS\t BURST TIME \t WAITING TIME\tTURNAROUND TIME\n");
+    for (i = 0; i < n; i++)
+        printf("%d\t\t%d\t\t%d\t\t%d\n", i + 1, ct[i], wa[i], tat[i]);
 
     return 0;
 }
 
-
 //output
-Enter the number of processes: 4
+Enter the number of processes: 3
+Enter Burst Time for process 1: 2
+Enter Burst Time for process 2: 6
+Enter Burst Time for process 3: 3
+Enter the time slice: 2
 
-Enter Burst Time for process 1: 24
+The Average Turnaround Time is: 7.33
+The Average Waiting Time is: 3.67
 
-Enter Burst Time for process 2: 8
-
-Enter Burst Time for process 3: 12
-
-Enter Burst Time for process 4: 6
-
-Enter the size of time slice: 4
-
-The Average Turnaround Time is -- 22.50
-The Average Waiting Time is -- 8.25
-    PROCESS  BURST TIME   WAITING TIME  TURNAROUND TIME
-    1         24           8             24
-    2         8            0             8
-    3         12           4             16
-    4         6            6             12
+PROCESS	 BURST TIME 	 WAITING TIME	TURNAROUND TIME
+1		    2		               0		     2
+2		    6	                   5		     11
+3		    3		               6		     9
